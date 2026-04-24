52637cc11e2986fbed22099466fa5303941a949a
felipeoliveiracustodio-lgtm <felipe.oliveira.custodio@escola.pr.gov.br>
2026-04-24 15:28:57 -0300

    css

--- script/script.js
+++ script/script.js
@@ -2,42 +2,28 @@ function salvarCarros(event) {
     //impedir de recarregar a página
     event.preventDefault();
 
-    let titulo = document.getElementById('title');
-    let preco = document.getElementById('preco');
-    let marca = document.getElementById('marca');
-    let modelo = document.getElementById('modelo');
+    let titulo = document.getElementById('title').value;
+    let preco = document.getElementById('preco').value;
+    let marca = document.getElementById('marca').value;
+    let modelo = document.getElementById('modelo').value;
 
     let cambioSelecionado = document.querySelector('input[name="marcha"]:checked');
 
     let cambio = cambioSelecionado ? cambioSelecionado.id : "Não informado";
 
-    let lista = document.getElementById('listarCarros');
-
-    let card = document.createElement('div');
-
-    card.style.border = "1px solid #ccc";
-    card.style.padding = "10px";
-    card.style.marginBottom = "10px";
-
-    card.innerHTML = '
-    <h3>${titulo}</h3>
-    <p><strong>Preço:</strong> R$ ${preco}</p>
-    <p><strong>Marca:</strong> R$ ${marca}</p>
-    <p><strong>Modelo:</strong> R$ ${modelo}</p>
-    <p><strong>Câmbio:</strong> R$ ${cambio}</p>
-    ';
-
-    lista.appendChild(card);
-
-    let carro = {
+        let carro = {
         titulo,
         preco,
         marca,
+        modelo, 
         cambio
     };
 
-    let carros = JSON.parse(localStorage.getItem("carros")) ||[];
+    let carros = JSON.parse(localStorage.getItem("carros")) || [];
     carros.push(carro);
     localStorage.setItem("carros", JSON.stringify(carros));
-}
 
+    adicionarNatela(carro);
+
+    document.querySelector("form").reset();
+}
